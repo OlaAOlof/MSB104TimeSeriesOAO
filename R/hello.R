fafa2efc26ce2c221d81716ce19798a3ee0d514f
OlaAOlof <ola.andre95@gmail.com>
2021-10-13 13:41:34 +0200

    Second commit

--- R/hello.R
+++ R/hello.R
@@ -13,9 +13,17 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+#´ This is a hello function with a name argument
+#´ @export
+hello <- function(name=NULL) {
+  tekst <- paste("Hello world! My name is", name)
+  print(tekst)
 }
 
-hello()
+
+
+
+
+
+
 
